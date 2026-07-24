d42f09edde90bf5e1f70ec24bb19c4908a5f61f2
heisenberg-611 <lnsaha1214@gmail.com>
2026-07-24 10:30:47 +0600

    fix: adjust feedback pill bottom positioning to prevent mobile overlay

--- src/components/PlatformFeedbackPill.tsx
+++ src/components/PlatformFeedbackPill.tsx
@@ -52,7 +52,7 @@ export function PlatformFeedbackPill() {
   };
 
   return (
-    <div className="fixed bottom-6 right-6 z-50 flex flex-col items-end">
+    <div className="fixed bottom-[90px] md:bottom-6 right-6 z-50 flex flex-col items-end">
       <AnimatePresence>
         {isOpen && (
           <motion.div
